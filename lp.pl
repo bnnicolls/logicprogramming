a5b472052c992cd3a7bbdc45e99a2c89ed03538c
Brandi Nicolls <brandi.nicolls@gmail.com>
2017-02-10 07:48:42 -0500

    Fixed Singleton Variable Errors

--- lp.pl
+++ lp.pl
@@ -1,5 +1,5 @@
 sum-up-numbers-simple([],0).           %base case
 sum-up-numbers-simple(L,N) :-
     L = (L1|L2),                       %Grabs First Item in List
-    sum-up-numbers-simple(L, N1),       %Recursive Call (till last element in list)
-    N is L1.                            %returns sum of numbers in list
+    sum-up-numbers-simple(L2, N1),      %Recursive Call (till last element in list)
+    N is N1 + L1.                       %returns sum of numbers in list
