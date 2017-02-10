ad462eb03d00c5dcef8c70af9fa72ec02751f5e6
Brandi Nicolls <brandi.nicolls@gmail.com>
2017-02-10 08:12:31 -0500

    Added Statement to Fix False

--- lp.pl
+++ lp.pl
@@ -1,5 +1,7 @@
 sum-up-numbers-simple([],0).           %base case
 sum-up-numbers-simple(L,N) :-
-    L = (L1|L2),                       %Grabs First Item in List
+
+    L = [L1|L2],                       %Grabs First Item in List
     sum-up-numbers-simple(L2, N1),      %Recursive Call (till last element in list)
-    N is N1 + L1.                       %returns sum of numbers in list
+    N is L1 + N1,
+    N >= 0.                       %returns sum of numbers in list
