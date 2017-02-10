43c3b753fd8eac8d7312f68b4223bec1a3648aef
Brandi Nicolls <brandi.nicolls@gmail.com>
2017-02-10 08:25:37 -0500

    add test case for letters.

--- lp.pl
+++ lp.pl
@@ -1,7 +1,9 @@
 sum-up-numbers-simple([],0).           %base case
-sum-up-numbers-simple(L,N) :-
-
-    L = [L1|L2],                       %Grabs First Item in List
+sum-up-numbers-simple(L,N) :-           %General Case
+    L = [L1|L2],                        %Grabs First Item in List
     sum-up-numbers-simple(L2, N1),      %Recursive Call (till last element in list)
-    N is L1 + N1,
-    N >= 0.                       %returns sum of numbers in list
+    N is L1+N1.                       %returns sum of numbers in list
+sum-up-numbers-simple(L,N) :-           %test case
+    L = [L1|L2],
+    \+number(L1),                       %if not a number
+    sum-up-numbers-simple(L2, N).       %continue recursive call
